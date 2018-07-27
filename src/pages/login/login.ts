e5d2471b39f7b6b52634afd04e7f160ad3a5ad6d
juan david   torres muñoz <juanda@MacBook-juanda.local>
2018-07-26 20:35:39 -0500

    lista navegacion

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SignInPage } from '../sign-in/sign-in';
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-login',
@@ -10,6 +12,17 @@ export class LoginPage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  gotosignin(){
+    this.navCtrl.push(SignInPage)
+    console.log("hey")
+  }
+
+  gotohome(){
+    this.navCtrl.push(HomePage)
+    console.log("hey")
+  }
+
 
 }
 
